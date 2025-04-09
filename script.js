9c98794022797908788aec100210b62ce8d60afa
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-09 17:32:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+function handleSubmit(event){
+	event.preventDefault();
+
+	//get form data
+	const firstname=document.forms["infoform"]["First Name"].value;
+	const lastname=document.forms["infoform"]["Last Name"].value;
+	const phone=document.forms["infoform"]["Phone Number"].value;
+	const email=document.forms["infoform"]["Email ID"].value;
+
+	const message=
+		"First Name: "+firstname+ "\n" +
+		"Last Name: "+lastname+"\n"+
+		"Phone Number: "+phone+"\n"+
+		"Email ID: "+email+"\n";
+
+	alert(message);
+
+
+
+
+
+
+	
+}
